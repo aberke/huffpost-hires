f2f007540ea8c311106d300a849db412e8a60531
Alex Berke <aberke13@us155648.local>
2013-10-24 23:58:46 -0400

    no more sqllite

--- project.clj
+++ project.clj
@@ -11,8 +11,7 @@
                  [environ "0.2.1"]
                  [com.cemerick/drawbridge "0.0.6"]
                  [org.clojure/java.jdbc "0.3.0-alpha5"]
-                 [postgresql "9.1-901.jdbc4"]
-                 [org.xerial/sqlite-jdbc "3.7.2"]]
+                 [postgresql "9.1-901.jdbc4"]]
   :min-lein-version "2.0.0"
   :plugins [[environ/environ.lein "0.2.1"]]
   :hooks [environ.leiningen.hooks]
